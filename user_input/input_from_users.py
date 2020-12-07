4710d424504990ae007f4b3d5dd38d0ecb0f5a5a
Jkarage <josephbkarage@gmail.com>
2020-12-07 14:19:25 +0300

    Update input_from_users.py

--- user_input/input_from_users.py
+++ user_input/input_from_users.py
@@ -3,6 +3,9 @@ Write a simple script that would take input from user using keyboard
 '''
 
 first_name = input("What is your first name?")
+
 surname = input("What is your surname: ")
+
 full_name =f"Your name is {first_name} " +f'{surname}'
+
 print(full_name)
